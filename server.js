e3c4591548de17be23a72d75fda7abb6e5e48466
omar salah <114373468+omarsalaheldieen@users.noreply.github.com>
2024-07-19 19:35:43 +0300

    feat: Add node

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Load employees
+app.get("/employees", (req, res) => {
+  fs.readFile("employees.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send("Error reading employees data");
+      return;
+    }
+    res.send(JSON.parse(data));
+  });
+});
+
+// Save new employee
+app.post("/employees", (req, res) => {
+  const newEmployee = req.body;
+  fs.readFile("employees.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send("Error reading employees data");
+      return;
+    }
+    const employees = JSON.parse(data);
+    employees.push(newEmployee);
+    fs.writeFile("employees.json", JSON.stringify(employees, null, 2), (err) => {
+      if (err) {
+        res.status(500).send("Error saving new employee");
+        return;
+      }
+      res.send(newEmployee);
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
